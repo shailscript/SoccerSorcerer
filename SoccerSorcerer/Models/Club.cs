2776848a3e23b94880c4feefdf92b84faba218af
Shailendra Shukla <shailendrashuklapro@gmail.com>
2020-03-12 11:24:32 -0400

    Added models

--- SoccerSorcerer/Models/Club.cs
+++ SoccerSorcerer/Models/Club.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Shailendra Shukla - 301057291
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,12 @@ namespace SoccerSorcerer.Models
 {
     public class Club
     {
+        public string Name { get; set; }
+        public string Manager { get; set; }
+        public string Stadium { get; set; }
+        public string Owner { get; set; }
+        public string Website { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
     }
 }
--- SoccerSorcerer/Models/Player.cs
+++ SoccerSorcerer/Models/Player.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Shailendra Shukla - 301057291
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,11 @@ namespace SoccerSorcerer.Models
 {
     public class Player
     {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Club { get; set; }
+        public int JerseyNumber { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
     }
 }
