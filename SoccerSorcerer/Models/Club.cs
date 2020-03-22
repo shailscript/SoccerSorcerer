057f16d1c932f45b2a97064225194a0f11086d0b
Shailendra Shukla <shailendrashuklapro@gmail.com>
2020-03-22 13:59:12 -0400

    Updated models for EF

--- SoccerSorcerer/Models/Club.cs
+++ SoccerSorcerer/Models/Club.cs
@@ -9,12 +9,16 @@ namespace SoccerSorcerer.Models
 {
     public class Club
     {
+#nullable enable
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Manager { get; set; }
         public string Stadium { get; set; }
         public string Owner { get; set; }
-        public string Website { get; set; }
+        public string? Website { get; set; }
         public string Location { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+#nullable disable
+        public ICollection<Player> Players { get; set; }
     }
 }
--- SoccerSorcerer/Models/Player.cs
+++ SoccerSorcerer/Models/Player.cs
@@ -9,11 +9,14 @@ namespace SoccerSorcerer.Models
 {
     public class Player
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Salary { get; set; }
-        public string Club { get; set; }
         public int JerseyNumber { get; set; }
         public string Position { get; set; }
         public string Description { get; set; }
+
+        public int ClubId { get; set; } // Foreign Key relationship, created as a shadow prop when not defined explicitly
+        public Club Club { get; set; } // Navigation Property
     }
 }
