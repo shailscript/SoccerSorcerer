2248d136c49bc5cbb00bd999da20b49420f40e5d
Shailendra Shukla <shailendrashuklapro@gmail.com>
2020-03-22 14:01:02 -0400

    Add Application Db Context

---
+++ SoccerSorcerer/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SoccerSorcerer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
