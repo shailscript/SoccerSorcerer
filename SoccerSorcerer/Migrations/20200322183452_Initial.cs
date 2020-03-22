d95dad320020a24626542814f05ad52122c67e7a
Shailendra Shukla <shailendrashuklapro@gmail.com>
2020-03-22 15:04:29 -0400

    Add and Update migration

---
+++ SoccerSorcerer/Migrations/20200322183452_Initial.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoccerSorcerer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clubs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Manager = table.Column<string>(nullable: false),
+                    Stadium = table.Column<string>(nullable: false),
+                    Owner = table.Column<string>(nullable: false),
+                    Website = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    JerseyNumber = table.Column<int>(nullable: false),
+                    Position = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ClubId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_Clubs_ClubId",
+                        column: x => x.ClubId,
+                        principalTable: "Clubs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ClubId",
+                table: "Players",
+                column: "ClubId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Clubs");
+        }
+    }
+}
