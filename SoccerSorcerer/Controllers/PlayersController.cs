031193d9ccf09fbb7cb49fd3f32505e167be3647
Shailendra Shukla <shailendrashuklapro@gmail.com>
2020-03-25 00:27:35 -0400

    Player Create Implemented

--- SoccerSorcerer/Controllers/PlayersController.cs
+++ SoccerSorcerer/Controllers/PlayersController.cs
@@ -5,19 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SoccerSorcerer.Models;
 
 namespace SoccerSorcerer.Controllers
 {
     public class PlayersController : Controller
     {
-        public IActionResult Index()
+        private IPlayerRepository repository;
+
+        public PlayersController(IPlayerRepository repo)
         {
-            return View();
+            repository = repo;
+        }
+        public IActionResult Index() => View(repository.Players);
+        
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Player player)
+        {
+            //FakeClubRepository.AddClub(c);
+            repository.AddPlayer(player);
+            return RedirectToAction("Index");
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+        public IActionResult Show() => View();
     }
 }
