80cf23ac53f08d907d8775cc66027def0e77a612
Shailendra Shukla <shailendrashuklapro@gmail.com>
2020-03-25 00:26:05 -0400

    Add Players Repository

---
+++ SoccerSorcerer/Models/EFPlayerRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerSorcerer.Models
+{
+    public class EFPlayerRepository : IPlayerRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFPlayerRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public void AddPlayer(Player player)
+        {
+            context.Players.Add(player);
+            context.SaveChanges();
+        }
+        public IQueryable<Player> Players => context.Players;
+    }
+}
